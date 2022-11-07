9d52bd05d7179a03d29de7155e2f1b63e2d8ecb3
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-07 10:42:34 +0100

    Add shop endpoints

--- server.js
+++ server.js
@@ -3,26 +3,23 @@ const path = require('path');
 
 const app = express();
 
+app.use('/admin', (req, res, next) => {
+  if(isAdmin()) next();
+  else res.send('Go away!');
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/index.html'));
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/about.html'));
+app.get('/cart', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/cart.html'));
 });
 
-app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/contact.html'));
+app.get('/admin/products', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/admin/products.html'));
 });
 
-app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/info.html'));
-});
-
-app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, './views/history.html'));
-});
-
-app.listen(8000, () => {
-  console.log(path.join(__dirname, 'Server is running on port: 8000'));
+app.get('/admin/payments', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/admin/payments.html'));
 });
