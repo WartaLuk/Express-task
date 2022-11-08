a6017f88516065cb1f9243b9d658daaf3fc37f88
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-08 18:57:01 +0100

    Add dark layout

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const path = require("path");
 const hbs = require("express-handlebars");
 
 const app = express();
-app.engine(".hbs", hbs());
+app.engine(
+  "hbs",
+  hbs({
+    extname: "handlebars",
+    layoutsDir: "./views/layouts",
+    defaultLayout: "main",
+  })
+);
+
 app.set("view engine", ".hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
@@ -13,23 +21,23 @@ app.use("/user", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("index" );
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about" );
+  res.render("about");
 });
 
 app.get("/contact", (req, res) => {
-  res.render("contact" );
+  res.render("contact");
 });
 
 app.get("/info", (req, res) => {
-  res.render("info" );
+  res.render("info");
 });
 
 app.get("/history", (req, res, next) => {
-  res.render("history" );
+  res.render("history", {layout: "dark"});
 });
 
 app.get("/hello/:name", (req, res) => {
@@ -37,9 +45,9 @@ app.get("/hello/:name", (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).render("404.html" );
+  res.status(404).render("404.html");
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port: 8000" );
+  console.log("Server is running on port: 8000");
 });
