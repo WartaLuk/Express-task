ddf51be9ed7b1121a8e96764dfcfd0b687877861
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-08 17:31:34 +0100

    Add handlebars

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -12,8 +15,8 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.use('/user', (req, res) => {
-  res.show('forbidden.html');
+app.use("/user", (req, res) => {
+  res.show("forbidden.html");
 });
 
 app.get("/", (req, res) => {
@@ -34,6 +37,10 @@ app.get("/info", (req, res) => {
 
 app.get("/history", (req, res, next) => {
   res.show("history.html");
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
