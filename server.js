c29b4c5c296f12d3b7eb11340ce1bcdb50e4f077
Łukasz Wartałowicz <wartalowicz.lukasz.praca@gmail.com>
2022-11-08 17:39:59 +0100

    Use handlebars in every html file

--- server.js
+++ server.js
@@ -1,52 +1,45 @@
 const express = require("express");
 const path = require("path");
-const hbs = require('express-handlebars');
+const hbs = require("express-handlebars");
 
 const app = express();
-app.engine('.hbs', hbs());
-app.set('view engine', '.hbs');
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine(".hbs", hbs());
+app.set("view engine", ".hbs");
 
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/user", (req, res) => {
-  res.show("forbidden.html");
+  res.render("forbidden", { layout: false});
 });
 
 app.get("/", (req, res) => {
-  res.show("index.html");
+  res.render("index", { layout: false});
 });
 
 app.get("/about", (req, res) => {
-  res.show("about.html");
+  res.render("about", { layout: false});
 });
 
 app.get("/contact", (req, res) => {
-  res.show("contact.html");
+  res.render("contact", { layout: false});
 });
 
 app.get("/info", (req, res) => {
-  res.show("info.html");
+  res.render("info", { layout: false});
 });
 
 app.get("/history", (req, res, next) => {
-  res.show("history.html");
+  res.render("history", { layout: false});
 });
 
 app.get("/hello/:name", (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render("hello", { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
-  res.status(404).show("404.html");
+  res.status(404).render("404.html", { layout: false});
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port: 8000");
+  console.log("Server is running on port: 8000", { layout: false});
 });
